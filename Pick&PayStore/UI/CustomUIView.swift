c2038ca4931818f8d415278deb80630d75aa38bf
sevaggap <sevag.gaprielian@gmail.com>
2022-06-28 17:50:40 -0400

    added Custom class files for the view, button and header, added info.plist, added custom blue color, added a testing storyboard

---
+++ Pick&PayStore/UI/CustomUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomUIView.swift
+//  Pick&PayStore
+//
+//  Created by Sevag Gaprielian on 2022-06-28.
+//
+
+import UIKit
+
+class CustomUIView: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [UIColor(named: "CustomBlue")!.cgColor, UIColor.white.cgColor, UIColor.white.cgColor]
+        layer.insertSublayer(gradient, at: 0)
+    }
+
+}
