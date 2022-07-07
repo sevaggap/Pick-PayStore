f96bc75b5d2f568b79d7796ab9387fc059d7b205
sevaggap <sevag.gaprielian@gmail.com>
2022-07-07 17:20:35 -0400

    renamed functions in CategoryService, assigned products to categories in CategoryService, updated function calls in HomeScreenViewController, added 60 products to CoreData  in ProductsService, updated viewWillAppear in UITestViewController

--- Pick&PayStore/Home Screen/Service/CategoryService.swift
+++ Pick&PayStore/Home Screen/Service/CategoryService.swift
@@ -10,12 +10,11 @@ class CategoryService{
 
 static var categoryServiceInstance = CategoryService()
     
-    func resetData()
-    {
+    func resetData() {
         CategoryDBHelper.categoryDBHelper.resetCategories()
     }
-func addData()
-{
+    
+    func addCategoryData() {
     CategoryDBHelper.categoryDBHelper.addCategory(name: "Electronics", image: "headphones", id: 1)
     CategoryDBHelper.categoryDBHelper.addCategory(name: "TV & Appliances", image: "tv-appliances", id: 2)
     CategoryDBHelper.categoryDBHelper.addCategory(name: "Womens Fashion", image: "women-clothes", id: 3)
@@ -28,13 +27,81 @@ func addData()
     CategoryDBHelper.categoryDBHelper.addCategory(name: "furniture ", image: "furniture", id: 10)
     
     CategoryDBHelper.categoryDBHelper.addCategory(name: "books ", image: "books", id: 12)
+    }
     
-    
-    
-}
-    func addProductData(){
-        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId:1, productId: 1)
-        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId:2, productId: 2)
+    func addProductsToCategories(){
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 1, productId: 1)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 1, productId: 2)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 1, productId: 3)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 1, productId: 4)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 1, productId: 5)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 2, productId: 6)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 2, productId: 7)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 2, productId: 8)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 2, productId: 9)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 2, productId: 10)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 3, productId: 11)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 3, productId: 12)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 3, productId: 13)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 3, productId: 14)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 3, productId: 15)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 4, productId: 16)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 4, productId: 17)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 4, productId: 18)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 4, productId: 19)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 4, productId: 20)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 5, productId: 21)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 5, productId: 22)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 5, productId: 23)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 5, productId: 24)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 5, productId: 25)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 6, productId: 26)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 6, productId: 27)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 6, productId: 28)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 6, productId: 29)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 6, productId: 30)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 7, productId: 31)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 7, productId: 32)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 7, productId: 33)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 7, productId: 34)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 7, productId: 35)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 8, productId: 36)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 8, productId: 37)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 8, productId: 38)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 8, productId: 39)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 8, productId: 40)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 9, productId: 41)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 9, productId: 42)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 9, productId: 43)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 9, productId: 44)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 9, productId: 45)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 10, productId: 46)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 10, productId: 47)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 10, productId: 48)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 10, productId: 49)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 10, productId: 50)
+
+//        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 11, productId: 51)
+//        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 11, productId: 52)
+//        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 11, productId: 53)
+//        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 11, productId: 54)
+//        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 11, productId: 55)
+
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 12, productId: 56)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 12, productId: 57)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 12, productId: 58)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 12, productId: 59)
+        CategoryDBHelper.categoryDBHelper.addProductToCategory(categoryId: 12, productId: 60)
+
     }
     
     
